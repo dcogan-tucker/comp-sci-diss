00069d1ef0de02a52fb6f6270fa5987db2926e25
Dominic Cogan-Tucker <dxc653@@student.bham.ac.uk>
2020-08-18 12:36:33 +0100

    friction adjustments

--- src/main/CubeSlopeDemo.java
+++ src/main/CubeSlopeDemo.java
@@ -31,10 +31,10 @@ public class CubeSlopeDemo extends Application
 				new Vector3f(-1f, 1f, -7.5f), 
 				new Vector3f(0, 0, 0), 
 				0.8f, 1);
-		slope.getComponent(Weight.class).restitution = 0.3f;
-		cube.getComponent(Weight.class).restitution = 0.3f;
-		slope.getComponent(Weight.class).friction = 0.3f;
-		cube.getComponent(Weight.class).friction = 0.3f;
+		slope.getComponent(Weight.class).restitution = 0.6f;
+		cube.getComponent(Weight.class).restitution = 0.2f;
+		slope.getComponent(Weight.class).friction = 0.4f;
+		cube.getComponent(Weight.class).friction = 0.4f;
 	}
 	
 	public static void main(String[] args)
